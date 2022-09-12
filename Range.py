512da9a6abf33a60f7571efb1212d35b0f22e104
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-12 12:48:51 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,21 @@
+# for i in range(5):
+#     print(i,end='')
+
+# for i in range(5):
+#     print(i,end=' ')
+
+# for i in range(1,20,1):
+#     print(i)
+
+# for i in range(1,20,2):
+#     print(i)
+
+# for i in range(0,20,2):
+#     print(i)
+
+# for i in range(20,1,-1):
+#     print(i)
+
+
+for i in range(10,1,-2):
+    print(i)
